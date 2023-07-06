b93d0a01920250cc1752deee979f631944342d0f
tinorusso1012 <valentinorusso2002@gmail.com>
2023-07-06 14:56:29 -0300

    Commit Prueba

---
+++ Ejercicio_2/Competidor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    public class Competidor
+    {
+        private string nombre;
+        private string club;
+        private int edad;
+        private int[,] puntaje;
+        private string[,] TirosAl10;
+
+
+        public Competidor(string nombre, int edad, string club, int rondas)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.club = club;
+            this.puntaje = new int[rondas, 5];
+            this.TirosAl10 = new string[rondas, 5];
+        }
+
+        public string Nombre { get { return nombre; } } 
+        public string Club { get { return club; } }
+        public int Edad { get { return edad; } }
+        public int[,] Puntaje { get { return puntaje; } }
+
+        public void RellenarPuntaje(int ronda, int numerotiro, int puntaje, bool x) {
+            Puntaje[ronda,numerotiro] = puntaje;
+            if (puntaje == 10 && x == true)
+            {
+                TirosAl10[ronda, numerotiro] = "X";
+            }
+        }
+
+    }
+}
---
+++ Ejercicio_2/Torneo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    public class Torneo
+    {
+        Competidor[] competidores;
+        int CantidadMayores = 0;
+        int CantidadMenores = 0;
+        int CantCompetidores = 0;
+        int CantRondas;
+        int RondaActual = 0;
+
+        public Torneo(int cantRondas)
+        {
+            competidores = new Competidor[100];
+            cantRondas = cantRondas;
+        }
+
+        public void AgregarCompetidor(string nombre,string club,int edad)
+        {
+            competidores[CantCompetidores] = new Competidor(nombre, edad, club,this.CantRondas);
+        }
+
+    }
+}
